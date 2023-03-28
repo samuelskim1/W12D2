b1bd3b1b6575f9a1573190505ad3b9b955355f25
Samuel Kim <samuelskim420@gmail.com>
2023-03-28 20:51:08 +0000

    phase 0 defining root functional component

---
+++ authenticate-me/frontend/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import configureStore from './store';
+
+const store = configureStore();
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
